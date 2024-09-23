ff72ef59146af85cfe41db776d85d6932e305ac4
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-09-23 19:57:04 +0500

    Weather App

--- app.js
+++ app.js
@@ -8,25 +8,31 @@ const weahterIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  let data = await response.json();
-  //   console.log(data);
-  document.querySelector(".city").innerHTML = `${data.name}`;
-  document.querySelector(".temp").innerHTML = `${Math.round(
-    data.main.temp
-  )} "°C"`;
-  document.querySelector(".humidity").innerHTML = `${data.main.humidity} %`;
-  document.querySelector(".wind").innerHTML = `${data.wind.speed} km/h`;
-
-  if (data.weather[0].main == "Clouds") {
-    weahterIcon.src = "images/clouds.png";
-  } else if (data.weather[0].main == "Clear") {
-    weahterIcon.src = "images/clear.png";
-  } else if (data.weather[0].main == "Rain") {
-    weahterIcon.src = "images/rain.png";
-  } else if (data.weather[0].main == "Drizzle") {
-    weahterIcon.src = "images/drizzle.png";
-  } else if (data.weather[0].main == "Mist") {
-    weahterIcon.src = "images/mist.png";
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    let data = await response.json();
+    //   console.log(data);
+    document.querySelector(".city").innerHTML = `${data.name}`;
+    document.querySelector(".temp").innerHTML = `${Math.round(
+      data.main.temp
+    )} "°C"`;
+    document.querySelector(".humidity").innerHTML = `${data.main.humidity} %`;
+    document.querySelector(".wind").innerHTML = `${data.wind.speed} km/h`;
+    if (data.weather[0].main == "Clouds") {
+      weahterIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weahterIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weahterIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weahterIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weahterIcon.src = "images/mist.png";
+    }
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
   }
 }
 
